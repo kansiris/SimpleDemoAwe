db315bcebe4811ade0cdb9988b5cb40854dbfb29
kansiris <41776281+kansiris@users.noreply.github.com>
2018-08-13 14:42:53 +0530

    Initial commit

---
+++ SimpleDemoAwe/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Omu.AwesomeMvc;
+using SimpleDemoAwe.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SimpleDemoAwe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategories()
+        {
+            var items = Db.Categories
+                .Select(o => new KeyContent(o.Id, o.Name));
+
+            return Json(items);
+        }
+
+        public ActionResult GetMeals(int? parent)
+        {
+            var items = Db.Meals.Where(o => o.Category.Id == parent)
+                .Select(o => new KeyContent(o.Id, o.Name));
+
+            return Json(items);
+        }
+
+        public ActionResult GetAllMeals()
+        {
+            var items = Db.Meals.Select(o => new KeyContent(o.Id, o.Name));
+
+            return Json(items);
+        }
+
+        public ActionResult GetMealsAuto(string v)
+        {
+            v = (v ?? "").ToLower().Trim();
+            var items = Db.Meals.Where(o => o.Name.ToLower().Contains(v))
+                .Take(5)
+                .Select(o => new KeyContent(o.Id, o.Name, false));
+
+            return Json(items);
+        }
+
+        public ActionResult GridGetItems(GridParams g)
+        {
+            var list = Db.Dinners.AsQueryable();
+
+            var gridModel = new GridModelBuilder<Dinner>(list, g)
+            {
+                Key = "Id", // needed when using EF, nesting, tree, client api
+                Map = o => new
+                {
+                    o.Id,
+                    o.Name,
+                    Date = o.Date.ToString("dd MMM yyyy"),
+                    CountryName = o.Country.Name,
+                    ChefName = o.Chef.FirstName + " " + o.Chef.LastName
+                } 
+            }.Build();
+
+            return Json(gridModel);
+        }
+
+        public ActionResult GetMealsAjaxList(string parent, int page)
+        {
+            const int PageSize = 10;
+            parent = parent ?? "";
+
+            var list = Db.Meals.Where(o => o.Name.ToLower().Contains(parent.ToLower())).OrderByDescending(o => o.Id);
+
+            return Json(new AjaxListResult
+            {
+                Items = list.Skip((page - 1) * PageSize).Take(PageSize).Select(o => new KeyContent(o.Id, o.Name)),
+                More = list.Count() > page * PageSize
+            });
+        }
+    }
+}
---
+++ SimpleDemoAwe/Controllers/MealLookupController.cs
@@ -0,0 +1,29 @@
+﻿using Omu.AwesomeMvc;
+using SimpleDemoAwe.Models;
+
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SimpleDemoAwe.Controllers
+{
+    public class MealLookupController : Controller
+    {
+        public ActionResult GetItem(int? v)
+        {
+            var o = Db.Meals.SingleOrDefault(f => f.Id == v) ?? new Meal();
+
+            return Json(new KeyContent(o.Id, o.Name));
+        }
+
+        public ActionResult Search(string search, int page)
+        {
+            search = (search ?? "").ToLower().Trim();
+            var list = Db.Meals.Where(f => f.Name.ToLower().Contains(search));
+            return Json(new AjaxListResult
+            {
+                Items = list.Skip((page - 1) * 7).Take(7).Select(o => new KeyContent(o.Id, o.Name)),
+                More = list.Count() > page * 7
+            });
+        }
+    }
+}
